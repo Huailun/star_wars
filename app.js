1fc8c9e73990e9a895d3dcd17c6ceeef326caa49
Ellen Liu <Huailun@users.noreply.github.com>
2018-05-09 22:02:52 -0600

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ function bindButton() {
             homeReq.addEventListener('load', function () {
               if (homeReq.status >= 200 && homeReq.status < 400) {
                 var planetResponse = JSON.parse(homeReq.responseText);
-                console.log(planetResponse);
                 home.innerText = planetResponse.name;
               } else {
                 console.log('Error in network request: ' + req.statusText);
@@ -70,9 +69,9 @@ function bindButton() {
                     if(newReq.status >= 200 && newReq.status < 400){
                       var newResponse = JSON.parse(newReq.responseText);
                       var movie = document.createElement('li');
-                      movie.textContent = newResponse.title + ' ' + newResponse.release_date;
+                      movie.innerHTML = newResponse.title + '</br>' + newResponse.release_date
                       movieList.appendChild(movie);
-                      console.log(movieList);
+
                       var list = document.getElementById('movieList');     // Get the target element
                        list.innerHTML = "";                            // Remove previous content
                        list.appendChild(movieList); // Append your generated UL 
@@ -100,7 +99,95 @@ function bindButton() {
 }
 
 
+//filmUrl = "https://api.themoviedb.org/3/search/movie?api_key=dc15e9c690891206eda183085e1a1b53&language=en-US&query=star+war"
+// XML request for posters
+var posters = new XMLHttpRequest();
+var imgObject;
 
-  
+
+/*** 
+function loadImage() {
+  //get the posters
+
+  var posterUrl = 'https://api.themoviedb.org/3/search/movie?api_key=dc15e9c690891206eda183085e1a1b53&language=en-US&query=star+war'
+  posters.open('GET', posterUrl, true);
+  posters.responseType = 'text';
+  posters.send(null);
+}*/
+
+//get the posters
+posters.open('GET', 'https://api.themoviedb.org/3/search/movie?api_key=dc15e9c690891206eda183085e1a1b53&language=en-US&query=star+war', true);
+posters.responseType = 'text';
+posters.send(null);
+
+posters.onload = function() {
+  if (posters.status === 200 && posters.status < 400) {
+    imgObject = JSON.parse(posters.responseText);
+    console.log(imgObject);
+    for ( let i = 0; i< imgObject.results.length; i++){
+      var imageTitle = imgObject.results[i].original_title;
+      var imagePath = imgObject.results[i].poster_path;
+      /**
+      A New Hope = http://image.tmdb.org/t/p/w500/btTdmkgIvOi0FFip1sPuZI2oQG6.jpg
+      attack of the clones = http://image.tmdb.org/t/p/w500/2vcNFtrZXNwIcBgH5e2xXCmVR8t.jpg
+      the phantom menance = http://image.tmdb.org/t/p/w500/n8V09dDc02KsSN6Q4hC2BX6hN8X.jpg
+      revenge of the sith = http://image.tmdb.org/t/p/w500/tgr5Pdy7ehZYBqBkN2K7Q02xgOb.jpg
+      the force awakens = http://image.tmdb.org/t/p/w500/weUSwMdQIa3NaXVzwUoIIcAi85d.jpg
+      the empire strike back = http://image.tmdb.org/t/p/w500/6u1fYtxG5eqjhtCPDx04pJphQRW.jpg
+      return of the jedi = http://image.tmdb.org/t/p/w500/jx5p0aHlbPXqe3AH9G15NvmWaqQ.jpg
+       */
+    }
+
+  } //end if
+} //end function
+
+//Get movie title 
+function showPosters(){
+
+  var posters = Array.from(document.querySelectorAll('#movieList>ol>li'),li => li.innerHTML)
+  var imgSrc = [];
+
+  for (var i = 0; i < posters.length; i++){
+    switch(posters[i]) {
+      case "A New Hope<br>1977-05-25":
+        imgSrc.push('http://image.tmdb.org/t/p/w342/btTdmkgIvOi0FFip1sPuZI2oQG6.jpg');
+      break;
+      case "Attack of the Clones<br>2002-05-16":
+        imgSrc.push('http://image.tmdb.org/t/p/w342/2vcNFtrZXNwIcBgH5e2xXCmVR8t.jpg');
+      break;
+      case "The Phantom Menace<br>1999-05-19":
+        imgSrc.push('http://image.tmdb.org/t/p/w342/n8V09dDc02KsSN6Q4hC2BX6hN8X.jpg');
+      break;
+      case "Revenge of the Sith<br>2005-05-19":
+        imgSrc.push('http://image.tmdb.org/t/p/w342/tgr5Pdy7ehZYBqBkN2K7Q02xgOb.jpg');
+      break;
+      case "Return of the Jedi<br>1983-05-25":
+        imgSrc.push('http://image.tmdb.org/t/p/w342/jx5p0aHlbPXqe3AH9G15NvmWaqQ.jpg');
+      break;
+      case "The Empire Strikes Back<br>1980-05-17":
+        imgSrc.push('http://image.tmdb.org/t/p/w342/6u1fYtxG5eqjhtCPDx04pJphQRW.jpg');
+      break;
+      case "The Force Awakens<br>2015-12-11":
+        imgSrc.push('http://image.tmdb.org/t/p/w342/weUSwMdQIa3NaXVzwUoIIcAi85d.jpg');
+      break;
+      default:
+        console.log('Sorry! No Images');
+  }
+    
+}
+console.log(imgSrc);
+
+$(document).ready(function(){  
+  for(var j=0 ; j < imgSrc.length ; j++) {
+    $('<div class="item"><img class="center-block img-responsive" src="'+ imgSrc[j] +'"><div class="carousel-caption"></div>   </div>').appendTo('.carousel-inner');
+    $('<li data-target="#carousel-example-generic" data-slide-to="'+j+'"></li>').appendTo('.carousel-indicators')
+
+  }
+  $('.item').first().addClass('active');
+  $('.carousel-indicators > li').first().addClass('active');
+  $('#carousel-example-generic').carousel();
+});
+   
+}
 
 
